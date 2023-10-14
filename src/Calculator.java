467c15ba3d6064d03ad537a9c050cda72f50ed83
OlenaMaliuhina <145293630+OlenaMaliuhina@users.noreply.github.com>
2023-10-14 18:54:10 +0200

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,18 +1,14 @@
 class Calculator {
     protected double result;
-
     public void add(double a, double b) {
         result = a + b;
     }
-
     public void subtract(double a, double b) {
         result = a - b;
     }
-
     public void multiply(double a, double b) {
         result = a * b;
     }
-
     public void divide(double a, double b) {
         if (b != 0) {
             result = a / b;
