13f50f039066123009b59bab59aef2a0bd061980
OlenaMaliuhina <omaliuhina@outlook.com>
2023-10-14 18:27:00 +0200

    hw7 master

---
+++ src/IngCalculator.java
@@ -0,0 +1,10 @@
+class EngCalculator extends Calculator {
+    public void squareRoot(double a) {
+        result = Math.sqrt(a);
+    }
+
+    @Override
+    public void displayResult() {
+        System.out.println("Result of IngCalculator - sqrt: " + result);
+    }
+}
